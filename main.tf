55cadba68b8658621bbe28980efeac35f5703ae2
chinelo-obitube <obitubec@gmail.com>
2021-12-11 13:50:51 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region     = "eu-west-2"
+  access_key = "AKIAVRVUFPRLUPDEVNJG"
+  secret_key = "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"
+}
+
+resource "aws_vpc" "development-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+      Name: "development"
+      vpc_env: "dev"
+  }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+  vpc_id     = aws_vpc.development-vpc.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "eu-west-2a"
+}
+
+data "aws_vpc" "existing_vpc" {
+    default = true
+}
+
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id     = data.aws_vpc.existing_vpc.id
+  cidr_block = "172.31.48.0/20"
+  availability_zone = "eu-west-2a"
+} 
+
+output "dev-vpc-id"{
+  value = aws_vpc.development-vpc.id
+}
+
+output "dev-subnet-id"{
+  value = aws_subnet.dev-subnet-1.id
+}
